49adc03cf671567b768a57181c914cb50204ba07
chrischristakis <chriscchristakis@gmail.com>
2024-01-13 16:07:13 -0500

    Init

---
+++ Game/Game.cpp
@@ -0,0 +1,22 @@
+#include "stdafx.h"
+
+#include <windows.h> 
+#include <math.h>  
+
+#include "app\app.h"
+
+void Init()
+{
+}
+
+void Update(float deltaTime)
+{
+}
+
+void Render()
+{	
+}
+
+void Shutdown()
+{
+}
